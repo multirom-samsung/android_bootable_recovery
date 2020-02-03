de749743c6759937db06dea3a98c0e660ce74b8f
Chaosmaster <chaosmaster@chaosv.de>
2020-02-03 15:38:02 +0100

    Rework progressbar logic, implement scoped/split progressbar
    Rework progressbar according to AOSP, this should fix various
    issues with progressbar.
    
    From AOSP:
    
       progress <frac> <secs>
           fill up the next <frac> part of of the progress bar over <secs> seconds. If <secs> is
           zero, use `set_progress` commands to manually control the progress of this segment of the
           bar.
    
       set_progress <frac>
           <frac> should be between 0.0 and 1.0; sets the progress bar within the segment defined by
           the most recent progress command.
    
    Change-Id: I7f92359046288f3529cb2e98e6d54d410a248f1d

--- gui/action.cpp
+++ gui/action.cpp
@@ -414,6 +414,8 @@ int GUIAction::flash_zip(std::string filename, int* wipe_cache)
 	int ret_val = 0;
 
 	DataManager::SetValue("ui_progress", 0);
+	DataManager::SetValue("ui_portion_size", 0);
+	DataManager::SetValue("ui_portion_start", 0);
 
 	if (filename.empty())
 	{
@@ -453,6 +455,8 @@ int GUIAction::flash_zip(std::string filename, int* wipe_cache)
 	// Done
 	DataManager::SetValue("ui_progress", 100);
 	DataManager::SetValue("ui_progress", 0);
+	DataManager::SetValue("ui_portion_size", 0);
+	DataManager::SetValue("ui_portion_start", 0);
 	return ret_val;
 }
 
@@ -535,6 +539,8 @@ void GUIAction::operation_start(const string operation_name)
 	time(&Start);
 	DataManager::SetValue(TW_ACTION_BUSY, 1);
 	DataManager::SetValue("ui_progress", 0);
+	DataManager::SetValue("ui_portion_size", 0);
+	DataManager::SetValue("ui_portion_start", 0);
 	DataManager::SetValue("tw_operation", operation_name);
 	DataManager::SetValue("tw_operation_state", 0);
 	DataManager::SetValue("tw_operation_status", 0);
