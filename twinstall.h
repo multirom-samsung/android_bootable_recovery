16eb87fced0724fc63fb9a5b24fa90c053a6070a
epicX <souravgain605@gmail.com>
2021-03-12 18:14:23 +0530

    Allow all extension while checking for digest verification
    Change-Id: Ie17582f144e0a5b5a68e2c6d88c26cb9f5a29ee6

--- twinstall.h
+++ twinstall.h
@@ -1,6 +1,6 @@
 #ifndef RECOVERY_TWINSTALL_H_
 #define RECOVERY_TWINSTALL_H_
 
-int TWinstall_zip(const char* path, int* wipe_cache, int mr_skip_comp_verify = 0);
+int TWinstall_zip(const char* path, int* wipe_cache, bool check_for_digest = false, int mr_skip_comp_verify = 0);
 
 #endif  // RECOVERY_TWINSTALL_H_
